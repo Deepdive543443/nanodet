8aebf43ed3a82775b6a00e12385eaf454267af50
justinfung <qf28@sussex.ac.uk>
2023-12-29 23:17:58 +0000

    Wars with multi output keeps going

--- 2ncnn/2ncnn_pnnx.py
+++ 2ncnn/2ncnn_pnnx.py
@@ -1,18 +1,40 @@
-import argparse, os
+import argparse, os, copy
 import pnnx
+import onnx
+import onnxsim
 import torch
 nn = torch.nn
 
 from nanodet.model.arch import build_model
 from nanodet.util import Logger, cfg, load_config, load_model_weight
+from nanodet.model.arch.one_stage_detector import OneStageDetector
+from nanodet.model.head.nanodet_plus_head import NanoDetPlusHead
+from nanodet.model.backbone import build_backbone
+from nanodet.model.fpn import build_fpn
 
-class Script(nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
+class LottaStageDetector(OneStageDetector):
+    def __init__(
+        self, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
 
     def forward(self, x):
-        return self.model.forward(x)
+        x = self.backbone(x)
+        if hasattr(self, "fpn"):
+            x = self.fpn(x)
+        if hasattr(self, "head"):
+            x = self.head.forward_detach(x)
+            x1 = x[0]
+            x2 = x[1]
+            x3 = x[2]
+            x4 = x[3]
+
+        return (
+            x1[:, :80, ...], x1[:, 80:, ...],
+            x2[:, :80, ...], x2[:, 80:, ...],
+            x3[:, :80, ...], x3[:, 80:, ...],
+            x4[:, :80, ...], x4[:, 80:, ...]
+        )
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -30,19 +52,25 @@ if __name__ == "__main__":
     load_config(cfg, args.config)
     logger = Logger(0, use_tensorboard=False)
 
-    model = build_model(cfg.model)
+    # model = build_model(cfg.model)
+    model = LottaStageDetector(
+        backbone_cfg=cfg.model.arch.backbone,
+        fpn_cfg=cfg.model.arch.fpn,
+        head_cfg=cfg.model.arch.head
+    )
     ckpt = torch.load(args.model, map_location=lambda storage, loc: storage)
     load_model_weight(model, ckpt, logger)
     model.to("cpu").eval()
-    script = Script(model)
 
     x = torch.randn(1, 3, 416, 416)
-    print(script(x).shape)
+    # print(model(x), len(model(x)))
+    for output in model(x):
+        print(output.shape)
 
     os.system('mkdir -p ncnn_model')
     os.system('mkdir -p raw_output')
     pnnx.export(
-        model = script,
+        model = model,
         ptpath = f"raw_output/{args.model_name}.pt",
         inputs = x,
         pnnxparam = f"raw_output/{args.model_name}.pnnx.param",
@@ -52,7 +80,19 @@ if __name__ == "__main__":
         ncnnparam = f"ncnn_model/{args.model_name}.param",
         ncnnbin = f"ncnn_model/{args.model_name}.bin",
         ncnnpy = f"ncnn_model/{args.model_name}_ncnn.py",
+        optlevel = 2
     )
+
+    # torch.onnx.export(
+    #     model,
+    #     x,
+    #     f"raw_output/{args.model_name}.onnx",
+    #     verbose=True,
+    #     keep_initializers_as_inputs=True,
+    #     opset_version=11,
+    #     # input_names=["data"],
+    #     # output_names=["output"],
+    # )
 
     if not args.print_log:
         os.system('rm debug*')
